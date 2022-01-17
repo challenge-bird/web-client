11ddaeb1fcb7a9912a6e7573b4e168bd8459a213
Hanseok <hasuikhs@gmail.com>
2022-01-17 22:30:54 +0900

    220117|Update 404 template

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -21,26 +21,28 @@ function Login(): JSX.Element {
   }
 
   return (
-    <main className={`${styles['form-signin']}`}>
-      <form onSubmit={onSubmit}>
-        <h1>challenge-bird</h1>
-        <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
+    <>
+      <main className={`${styles['form-signin']}`}>
+        <form onSubmit={onSubmit}>
+          <h1>challenge-bird</h1>
+          <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
 
-        <div className="form-floating">
-          <input id="userid" className="form-control" type="text" placeholder="아이디"
-            value={userid} onChange={(e: React.ChangeEvent<HTMLInputElement>) => setUserid(e.target.value)} />
-          <label htmlFor="userid">ID</label>
-        </div>
+          <div className="form-floating">
+            <input id="userid" className="form-control" type="text" placeholder="아이디"
+              value={userid} onChange={(e: React.ChangeEvent<HTMLInputElement>) => setUserid(e.target.value)} />
+            <label htmlFor="userid">ID</label>
+          </div>
 
-        <div className="form-floating">
-          <input id="password" className="form-control" type="password" placeholder="비밀번호"
-            value={password} onChange={(e: React.ChangeEvent<HTMLInputElement>) => setPassword(e.target.value)} /><br />
-          <label htmlFor="password">Password</label>
-        </div>
+          <div className="form-floating">
+            <input id="password" className="form-control" type="password" placeholder="비밀번호"
+              value={password} onChange={(e: React.ChangeEvent<HTMLInputElement>) => setPassword(e.target.value)} /><br />
+            <label htmlFor="password">Password</label>
+          </div>
 
-        <button className="w-100 btn btn-lg btn-primary" type="submit">로그인</button>
-      </form>
-    </main>
+          <button className="w-100 btn btn-lg btn-primary" type="submit">로그인</button>
+        </form>
+      </main>
+    </>
   )
 }
 

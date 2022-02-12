17760d9627cb9fe54d26d2a8174b6d5042b38f60
hanseok kim <hasuikhs@gmail.com>
2022-02-12 19:45:58 +0900

    220212|Modify navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -18,14 +18,7 @@ function Navbar(): JSX.Element {
               <li className="nav-item">
                 <a className="nav-link" href="/link">Link</a>
               </li>
-              <li className="nav-item">
-                <a className="nav-link disabled">Disabled</a>
-              </li>
             </ul>
-            <form className="d-flex">
-              <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                <button className="btn btn-outline-success" type="submit">Search</button>
-            </form>
           </div>
         </div>
       </nav>
